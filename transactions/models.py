dd8e463502ca5258668045ef5bb9ef4f8d71f516
jihadul4825 <jihadul4825@gmail.com>
2025-08-26 23:45:44 +0600

    update transactions model

--- transactions/models.py
+++ transactions/models.py
@@ -8,10 +8,10 @@ from .constants import TRANSACTION_TYPES
 class Transaction(models.Model):
     account = models.ForeignKey(UserBankAccount, on_delete=models.CASCADE, related_name='transactions')
     amount = models.DecimalField(max_digits=12, decimal_places=2)
-    balance_after_transaction = models.DecimalField(max_digits=12, decimal_places=2)
+    balance_after_transaction = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
     transaction_type = models.IntegerField(choices=TRANSACTION_TYPES, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
-    loan_approve = models.BooleanField(null=True, default=False)
+    loan_approve = models.BooleanField(default=False)
     
     # This line allows a loan repayment to reference the original loan
     # original_loan = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='repayments')
